0db2e06a1ebba445f85b021364bb60449c7ea844
artdotlis <artdotlis@pm.me>
2025-11-14 13:58:44 +0000

    fix: add path to agreed cookie

--- strinf/frontend/src/ts/functions/cookie/banner.ts
+++ strinf/frontend/src/ts/functions/cookie/banner.ts
@@ -3,7 +3,7 @@ const F_COOKIE = 'agreed';
 function setFirstAgreed(): void {
     const expTime = new Date();
     expTime.setTime(expTime.getTime() + 180 * 24 * 3600 * 1000);
-    const cStr = `${F_COOKIE}=true;Secure;SameSite=Strict;expires=${expTime}`;
+    const cStr = `${F_COOKIE}=true;Secure;SameSite=Strict;Path=/;expires=${expTime};`;
     document.cookie = cStr;
 }
 
